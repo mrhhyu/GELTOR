8351cd634afa0af30342de979df224b07eb8b456
MRH <74174954+mrhhyu@users.noreply.github.com>
2023-04-28 18:27:02 +0900

    Update AdaSim_star_SE.py

--- AdaSim_star_SE.py
+++ AdaSim_star_SE.py
@@ -42,7 +42,7 @@ def compute_AdaSim_star (graph='', iterations=0, damping_factor=0.8, topK=0, los
     nodes.clear()     
     if topK == -1: ## calculating topK for the input graph
         topK = round(G.number_of_edges()/G.number_of_nodes()) * 8
-    topK = topK +1 ## since a node itself is considered in topK list
+    topK = topK +1 ## a node itself is also considered in the topK list as the most similar one to itself
     degrees = adj.sum(axis=0).T   # V*1 matrix (a column vector of size V)        
     weights = csr_matrix(1/np.log(degrees+math.e))  # keep weights of nodes; V*1 matrix;
     weight_matrix = csr_matrix(adj.multiply(weights)) # V*V matrix; column i have the weight of i's in-neighbors
