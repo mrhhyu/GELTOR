70742ccf8e06a27c6597dc4f9a24b7619ef7bbe4
MRH <74174954+mrhhyu@users.noreply.github.com>
2023-08-08 11:41:00 +0900

    Update AdaSim_star_SE.py

--- AdaSim_star_SE.py
+++ AdaSim_star_SE.py
@@ -60,7 +60,7 @@ def compute_AdaSim_star (graph='', iterations=0, damping_factor=0.8, topK=0, los
     if iterations == 1:
         if loss=='listMLE_topK':
             top_indices,top_simvals = prData.get_listMLE_topK(result_matrix,topK)
-            return top_indices,top_simvals
+            return top_indices,top_simvals,topK-1
 
     weight_matrix = normalize(weight_matrix, norm='l1', axis=0) # column normalized weight_matrix    
     for itr in range (2, iterations+1):           
