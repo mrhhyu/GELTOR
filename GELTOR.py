1841bf8cb2ed527563656b617c9025a72fdb6c51
MRH <74174954+mrhhyu@users.noreply.github.com>
2023-08-09 14:44:18 +0900

    Update GELTOR.py

--- GELTOR.py
+++ GELTOR.py
@@ -38,12 +38,6 @@ def get_model(dim,out_len, learning_rate, reg_rate):
 
 def LTRG(args):
     print()
-    if args.topk_mnl and args.topk <= 0:
-        print('ERROR: topK is not valid ... please input the topK value!')
-        return
-    if args.bch_mnl and args.bch <= 0:
-        print('ERROR: batch size is not valid ... please input the batch size!')
-        return
     if not os.path.exists(args.graph):
         print('ERROR: graph is invalid ...!')
         return
@@ -54,14 +48,14 @@ def LTRG(args):
         os.environ["CUDA_VISIBLE_DEVICES"] = "-1" ## disabling GPU
 
     print('================================================================= Similarity Computation =================================================================')
-    if args.topk_mnl:
-        top_indices,top_simvals,args.topk = compute_AdaSim_star(graph=args.graph, iterations=args.itr, damping_factor=0.4, topK=args.topk, loss='listMLE_topK')#[0]
-    else:
-        top_indices,top_simvals,args.topk = compute_AdaSim_star(graph=args.graph, iterations=args.itr, damping_factor=0.4, topK=-1, loss='listMLE_topK')#[0]
-        args.topk = top_indices.shape[1]-1
+    top_indices,top_simvals,args.topk = compute_AdaSim_star(graph=args.graph, iterations=args.itr, damping_factor=0.4, topK=args.topk, loss='listMLE_topK')#[0]
 
-    if not args.bch_mnl: # calculating batch size for the input graph
+    if not args.bch==-1: # calculating batch size for the input graph
         args.bch = pow(2, round(math.log2(len(top_indices)*0.05)))
+        if args.bch<1: ## when graph has few nodes
+            print('*** Computed bch values ({}) is invalid! set the bch value manually ***'.format(args.bch))
+            print('')
+            return                             
 
     print('==================================================================== GELTOR ARGUMENTS ====================================================================')
     print(args,'\n')
@@ -94,19 +88,17 @@ def LTRG(args):
     emb_file.close()
     print('The embedding result is written in the file ....')
 
-def parse_args(graph='',dataset_name='',result_dir='output/', dimension=128, topk_mnl=False, topK=-1, iterations=5, epochs=300, bch_mnl=False, batch_size=-1, learning_rate=0.0025, reg_rate=0.001, early_stop=True, wait_thr=20, gpu_on=True):
+def parse_args(graph='',dataset_name='',result_dir='output/', dimension=128, topK=-1, iterations=5, epochs=300, batch_size=-1, learning_rate=0.0025, reg_rate=0.001, early_stop=True, wait_thr=20, gpu_on=True):
 
     parser = ArgumentParser(description="Run GELTOR, A Graph Embedding Method based on Learning to Rank.")
     parser.add_argument('--graph', nargs='?', default=graph, help='Input graph')
     parser.add_argument('--dataset_name', nargs='?', default=dataset_name, help='dataset name')
     parser.add_argument('--result_dir', nargs='?', default=result_dir, help='Destination to save the embedding result, default is "output/" in the root directory')
     parser.add_argument('--dim', type=int, default=dimension, help='The embedding dimension, default is 128')
-    parser.add_argument('--topk_mnl', type=bool, default=topk_mnl, help='The flag indicating to input topK value manually or to calculate it automatically, default is False')
-    parser.add_argument('--topk', type=int, default=topK, help='Number of nodes in topK, default is -1')
+    parser.add_argument('--topk', type=int, default=topK, help='Number of nodes in topK; default is -1 (i.e., topK is automatically computed as explained in the paper), otherwise input the value manually')
     parser.add_argument('--itr', type=int, default=iterations, help='Number of Iterations to compute AdaSim*, default is 5')
     parser.add_argument('--epc', type=int, default=epochs, help='Number of Epochs for training, default is 300')
-    parser.add_argument('--bch_mnl', type=bool, default=bch_mnl, help='The flag indicating to input batch size manually or to calculate it automatically, default is False')
-    parser.add_argument('--bch', type=int, default=batch_size, help='Number of examples in a batch, default is -1')
+    parser.add_argument('--bch', type=int, default=batch_size, help='batch size, default is -1 (i.e., it is automatically computed as explained in the paper), otherwise input the value manually')
     parser.add_argument('--lr', type=float, default=learning_rate, help='Learning rate, default is 0.0025')
     parser.add_argument('--reg', type=float, default=reg_rate, help='Regularization parameter which is suggested to be 0.001 and 0.0001 with directed and undirected graphs, respectively; default is 0.001')
     parser.add_argument('--early_stop', type=bool, default=early_stop, help='The flag indicating to stop the training process if the loss stops improving, default is True')
