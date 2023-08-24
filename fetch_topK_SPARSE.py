e47689644e817902480c285bb1b3992f48867d0b
Masoud Reyhani Hamedani <74174954+mrhhyu@users.noreply.github.com>
2023-08-24 23:55:57 +0900

    Update fetch_topK_SPARSE.py

--- fetch_topK_SPARSE.py
+++ fetch_topK_SPARSE.py
@@ -11,7 +11,6 @@ def get_listMLE_topK(result_matrix, topK):
     '''
         @return: top_indices: |V|*topK matrix contains indices of topK nodes to each node
     '''      
-     
     top_indices = np.zeros((result_matrix.shape[0],topK),dtype='int32')
     for target_node in range (0,result_matrix.shape[0]):
         target_node_res_sorted = np.argsort(result_matrix[target_node,:].toarray()[0], axis=0)[::-1][:topK]  ## sorting the indices on descending order of similarity values and return the topk
